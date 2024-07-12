dc05f2ae672914be2b3fb138b64a02aba3a4c987
vitoria <vitoriarntrindade@gmail.com>
2024-07-12 19:46:37 -0400

    add delete router

--- controller/athlete.py
+++ controller/athlete.py
@@ -12,7 +12,6 @@ from models.training_center import TrainingCenterModel
 from contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
 
-
 router = APIRouter()
 
 
@@ -26,7 +25,6 @@ async def post(
         db_session: DatabaseDependency,
         athlete_in: AthleteIn = Body(...)
 ):
-
     category_name = athlete_in.category.name
     training_center_name = athlete_in.training_center.name
 
@@ -99,11 +97,10 @@ async def query(id: UUID4, db_session: DatabaseDependency) -> AthleteOut:
     response_model=AthleteOut
 )
 async def patch(
-    id: UUID4,
-    db_session: DatabaseDependency,
-    atleta_up: AthleteUpdate = Body(...)
+        id: UUID4,
+        db_session: DatabaseDependency,
+        atleta_up: AthleteUpdate = Body(...)
 ) -> AthleteOut:
-
     athlete = (
         await db_session.execute(select(AthleteModel).filter_by(id=id))).scalars().one_or_none()
 
@@ -116,9 +113,29 @@ async def patch(
     athlete_update = atleta_up.model_dump(exclude_unset=True)
 
     for key, value in athlete_update.items():
-        setattr(athlete,  key, value)
+        setattr(athlete, key, value)
 
     await db_session.commit()
     await db_session.refresh(athlete)
 
     return athlete
+
+
+@router.delete(
+    '/{id}',
+    summary='Deletar um Atleta pelo id',
+    status_code=status.HTTP_204_NO_CONTENT
+)
+async def delete(id: UUID4, db_session: DatabaseDependency) -> None:
+    athlete: AthleteOut = (
+        await db_session.execute(select(AthleteModel).filter_by(id=id))
+    ).scalars().first()
+
+    if not athlete:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Atleta não encontrado no id: {id}'
+        )
+
+    await db_session.delete(athlete)
+    await db_session.commit()
